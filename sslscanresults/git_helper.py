e8ca0b5fe012ee5080a4dc29bf5485699d8029e6
Manoj Kumar <manjku@fb.com>
2022-07-29 18:04:45 +0100

    added git

--- sslscanresults/git_helper.py
+++ sslscanresults/git_helper.py
@@ -5,19 +5,28 @@ import shutil
 PATH_OF_GIT_REPO = r'/tmp/SSLLab_hosts_and_report'
 SSLLab_hosts_and_report_GIT_REPO = "https://github.com/manjku/SSLLab_hosts_and_report.git"
 COMMIT_MESSAGE = 'New reports'
+DESTINATION_PATH_OF_GIT_REPO = os.path.join(PATH_OF_GIT_REPO, ".git")
 
+def clone_report_repo():
+    try:
+        print(f"Removing any existing Repo {PATH_OF_GIT_REPO}") 
+        shutil.rmtree("/tmp/SSLLab_hosts_and_report", ignore_errors=True)
+        print(f"Cloning Report repo at {PATH_OF_GIT_REPO}")
+        Repo.clone_from(SSLLab_hosts_and_report_GIT_REPO, PATH_OF_GIT_REPO)
+        print(f"Successfully Cloned rep at [{PATH_OF_GIT_REPO}]")
+    except Exception as Err:
+        print(f"ERROR: Could not clone the Report repo at {PATH_OF_GIT_REPO} due to error: \nErr")
+        return 1
+   
 def git_push():
     try:
-        DESTINATION_PATH_OF_GIT_REPO = os.path.join(PATH_OF_GIT_REPO, ".git")
-        shutil.rmtree("/tmp/SSLLab_hosts_and_report", ignore_errors=True)
-        Repo.clone_from(SSLLab_hosts_and_report_GIT_REPO, PATH_OF_GIT_REPO)
-        os.system("touch /tmp/SSLLab_hosts_and_report/test6")
         repo = Repo(DESTINATION_PATH_OF_GIT_REPO)
         repo.git.add("--all")
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
-    except:
-        print('Some error occured while pushing the code')    
+        print(f"Successfully pushed new Reports at [{PATH_OF_GIT_REPO}]")
+    except Exception as Err:
+        print(f"ERROR: Could not push the Report repo at {PATH_OF_GIT_REPO} due to error: \nErr")
+        return 1
 
-git_push()
--- sslscanresults/sslscan-results.py
+++ sslscanresults/sslscan-results.py
@@ -8,13 +8,18 @@ from datetime import datetime
 from helper_modules import get_ssllab_scan_results, SUMMARY_COLUMNS
 from csv_to_html_table import csv_to_html_table
 from email_helper import send_email, RECEIVER_EMAIL
+from git_helper import clone_report_repo, git_push
 
 DOMAINS_SUMMARY_CSV = "/tmp/SSLLab_hosts_and_report/Reports/csv_reports/domains_summary.csv"
 HTML_TABLE_LOCATION = "/tmp/SSLLab_hosts_and_report/Reports/html_reports/table.html"
 DOMAIN_NAMES_FILE = "/tmp/SSLLab_hosts_and_report/domain_names.yaml"
 def main():
     print(SUMMARY_COLUMNS)
-   
+    
+    if (clone_report_repo()):
+        print("Exiting as Report Repo could not be cloned")   
+        return 1
+
     receiver_email = RECEIVER_EMAIL
     if len(sys.argv) == 2:
         receiver_email = sys.argv[1]
@@ -39,6 +44,10 @@ def main():
 
     csv_to_html_table(domain_summary_csv_with_date, html_table_location_with_date)
     send_email(html_table_location_with_date, receiver_email=receiver_email)
+    
+    if (git_push()):
+        print("Exiting as Report Repo could not be Pushed")
+        return 1
 
 if __name__ == "__main__":
     sys.exit(main())
